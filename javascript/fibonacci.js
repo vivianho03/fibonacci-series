fcb8bb1a647223e6cc3b7d6481026002fab62f65
Vivian Ho <ho.vivian89@gmail.com>
2023-01-14 00:11:48 -0800

    finished

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,9 @@
 function fibonacci(num) {
-  // type your code here
+  return num < 1 ? 0
+       : num <= 2 ? 1
+       : fibonacci(num - 1) + fibonacci(num - 2)
 }
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -22,3 +25,27 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// Write a function that accepts a number n
+// and returns the nth number in the Fibonacci sequence
+
+// 1, 1, 2, 3, 5, 8, 13 ...
+
+//Non-recursive version
+
+// function fib(n) {
+//   if (n < 3){
+//     return 1;
+//   }
+//   //two initial numbers
+//   let previousNum = 1; //1
+//   let currentNum = 1; //1
+
+//   //i = 2
+//   for (let i = 2; i < n; i++) {
+//     const next = previousNum + currentNum; // 1 + 1
+//     previousNum = currentNum; // 1
+//     currentNum = next; // 2
+//   }
+//   return currentNum;
+// }
